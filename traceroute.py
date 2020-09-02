b115b77b6511ac41d04f61f50d8607a318629ac9
dolliars <dolly.lanuza@gmail.com>
2020-09-02 00:04:58 -0400

    Implement traceroute

--- traceroute.py
+++ traceroute.py
@@ -1,9 +1,14 @@
 import sys
 from scapy.all import sr1, IP, ICMP
-
 x = 30
-
 for i in range(x):
     pkt=IP(ttl=x)
     pkt.dst="8.8.8.8"
-    sr1(pkt)
+    ans = sr1(pkt/ICMP())
+    if ans is None:
+        break
+    elif ans.type == 3:
+        print ("Done!", ans.src)
+        break
+    else:
+        print ("Hop %d: " % i , ans.src)
